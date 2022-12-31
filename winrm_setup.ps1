98aad1945459921ec6179d232256cb2cc4d4f764
Vincent Akena <vakena@outlook.com>
2022-12-31 11:20:03 +1100

    added new powershell scrips to configure windows

---
+++ winrm_setup.ps1
@@ -0,0 +1,14 @@
+$url = "https://raw.githubusercontent.com/jborean93/ansible-windows/master/scripts/Install-WMF3Hotfix.ps1"
+$file = "$env:temp\Install-WMF3Hotfix.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -Verbose
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file
+
+winrm enumerate winrm/config/Listener
--- wsl_ubuntu_setup.py
+++ wsl_ubuntu_setup.py
@@ -6,18 +6,31 @@ import os
 import logging
 
 # Configure logging to file
-logging.basicConfig(level=logging.DEBUG, filename="wsl_ubuntu_setup.log",
+logging.basicConfig(level=logging.DEBUG, filename="log.log",
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', filemode='w')
 
 
+# Update OS packages after initial WSL setup
 def update_os():
-    update_log = os.system(
+    os.system(
         'echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y')
-    logging.info(update_log)
+
+
+# Removes the base version of ansible that comes with Ubuntu - installs the latest version
+def install_ansible():
+    os.system('echo vakena | sudo -S sudo apt remove ansible')
+    os.system('echo vakena | sudo -S sudo apt --purge autoremove')
+    os.system('echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y')
+    os.system('echo vakena | sudo -S sudo apt -y install software-properties-common')
+    os.system('echo vakena | sudo -S sudo apt-add-repository ppa:ansible/ansible')
+    os.system('echo vakena | sudo -S sudo apt install ansible')
 
 
 def main():
-    pass
+    os.system('echo ### Updating OS packages ###')
+    update_os()
+    os.system('echo ### Installing latest version of Ansible ###')
+    install_ansible()
 
 
 if __name__ == "__main__":
