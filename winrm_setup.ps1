4a657fb2acdba690024adcda89939095214f025f
Vincent Akena <vakena@outlook.com>
2022-12-31 11:33:25 +1100

    updated winrm with commands from documentation

--- winrm_setup.ps1
+++ winrm_setup.ps1
@@ -11,4 +11,22 @@ $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 powershell.exe -ExecutionPolicy ByPass -File $file
 
+Write-Host("Getting Listener Configs")
 winrm enumerate winrm/config/Listener
+
+# Get thumbprint from previous command
+$thumbprint = "get thumbprint"
+Get-ChildItem -Path cert:\LocalMachine\My -Recurse | Where-Object { $_.Thumbprint -eq $thumbprint } | Select-Object *
+
+$selector_set = @{
+    Address   = "*"
+    Transport = "HTTPS"
+}
+$value_set = @{
+    CertificateThumbprint = "get thumbprint"
+}
+
+New-WSManInstance -ResourceURI "winrm/config/Listener" -SelectorSet $selector_set -ValueSet $value_set
+
+winrm get winrm/config/Service
+winrm get winrm/config/Winrs
