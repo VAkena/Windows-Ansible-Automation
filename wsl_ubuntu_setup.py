ceec8daeb23c8b119a8623d138f59d9c41b7d7f8
Vincent Akena <vakena@outlook.com>
2023-01-01 15:18:44 +1100

    added logging to wsl script, cleaned up logic

--- wsl_ubuntu_setup.py
+++ wsl_ubuntu_setup.py
@@ -1,7 +1,3 @@
-# Author: Vincent Akena
-# Date: December 31, 2022
-# Description: Setup the necessary ansible components on WSL Ubuntu 22.04
-
 import os
 import logging
 import time
@@ -14,27 +10,50 @@ logging.basicConfig(level=logging.DEBUG, filename="log.log",
 
 # Update OS packages after initial WSL setup
 def update_os():
-    os.system(
-        'echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y')
+    commands = [
+        "echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y"]
+
+    # Take each command and split it before executing
+    for c in commands:
+        split_command = c.split()
+        run = subprocess.run(split_command, capture_output=True)
+        # Logs command that ran and the exit code - exit code 0 means it ran successfully
+        logging.info(run)
 
 
 # Removes the base version of ansible that comes with Ubuntu - installs the latest version
 def install_ansible():
-    os.system('echo vakena | sudo -S sudo apt remove ansible')
-    os.system('echo vakena | sudo -S sudo apt --purge autoremove')
-    os.system('echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y')
-    os.system('echo vakena | sudo -S sudo apt -y install software-properties-common')
-    os.system('echo vakena | sudo -S sudo apt-add-repository ppa:ansible/ansible')
-    os.system('echo vakena | sudo -S sudo apt install ansible -y')
+    # List all commands required for task
+    commands = ["echo vakena | sudo -S sudo apt remove ansible",
+                "echo vakena | sudo -S sudo apt --purge autoremove",
+                "echo vakena | sudo -S sudo apt-get update && sudo apt upgrade -y",
+                "echo vakena | sudo -S sudo apt -y install software-properties-common",
+                "echo vakena | sudo -S sudo apt-add-repository ppa:ansible/ansible",
+                "echo vakena | sudo -S sudo apt install ansible -y"]
+
+    # Take each command and split it before executing
+    for c in commands:
+        split_command = c.split()
+        run = subprocess.run(split_command, capture_output=True)
+        # Logs command that ran and the exit code - exit code 0 means it ran successfully
+        logging.info(run)
 
 
 def replace_ansible_hosts():
-    # os.system('echo vakena | sudo -S sudo cp -f ./hosts /etc/ansible/hosts')
-    command = subprocess.run(["ls", "-l", "./"], capture_output=True)
-    logging.info(command)
+    commands = ["echo vakena | sudo -S sudo cp -f ./hosts /etc/ansible/hosts"]
+
+    # Take each command and split it before executing
+    for c in commands:
+        split_command = c.split()
+        run = subprocess.run(split_command, capture_output=True)
+        # Logs command that ran and the exit code - exit code 0 means it ran successfully
+        logging.info(run)
 
 
 def main():
+    # Show titles for each task that will be run
+    # Give user time to see what's about to happen
+
     os.system('echo UPDATING ALL OS PACKAGES')
     time.sleep(3)
     update_os()
@@ -46,6 +65,19 @@ def main():
     os.system('echo REPLACING THE ANSIBLE HOSTS FILE')
     time.sleep(3)
     replace_ansible_hosts()
+
+    # Check status codes in the log file for what commands succeeded
+    file = open("./log.log", "r")
+    data = file.read()
+    # Return code 0 means that our commands executed successfully
+    status_code_count = data.count("returncode=0")
+
+    # We check for 8 because we ran 8 commands above
+    if status_code_count == 8:
+        logging.info("Success return code count is: %d", status_code_count)
+    else:
+        logging.error(
+            "Not all commands executed successfully. %d commands executed successfully. Please chect what commands failed", status_code_count)
 
 
 if __name__ == "__main__":
